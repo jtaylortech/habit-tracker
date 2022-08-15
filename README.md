810520ecf5bb9237cc491ac55df43601886dc9ff
Jarred Taylor <jarrede20@gmail.com>
2022-08-15 18:55:01 -0400

    created the graph to track my reading habit and implemented an auth w/ http header

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # habit-tracker
+
+Habit Tracker to track how many pages I read a day, https://pixe.la/v1/users/jtaylortech/graphs/graph1.html
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+from urllib import request
 import requests
 from credentials import *
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 jt_pixela_token = jt_pixela_api
+USERNAME = "jtaylortech"
 
 
 user_params = { 
     "token": jt_pixela_api,
-    "username": "jtalortech",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response =requests.post(url=pixela_endpoint,json=user_params)
+# response =requests.post(url=pixela_endpoint,json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graphs_config = {
+    "id": "graph1",
+    "name": "Pages Read",
+    "unit": "pages",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": jt_pixela_api 
+}
+
+response = requests.post(url=graph_endpoint, json=graphs_config, headers=headers)
 print(response.text)
