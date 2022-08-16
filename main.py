2c30013989f0f30fb31d3009dcec6e9ccd76706b
Jarred Taylor <jarrede20@gmail.com>
2022-08-15 20:14:21 -0400

    used another post request to push data to pixela and populate pixels

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 jt_pixela_token = jt_pixela_api
 USERNAME = "jtaylortech"
+READING_GRAPH = "graph1"
 
 
 user_params = { 
@@ -20,8 +21,8 @@ user_params = {
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-graphs_config = {
-    "id": "graph1",
+graphs_config = { 
+    "id": READING_GRAPH,
     "name": "Pages Read",
     "unit": "pages",
     "type": "float",
@@ -32,5 +33,16 @@ headers = {
     "X-USER-TOKEN": jt_pixela_api 
 }
 
-response = requests.post(url=graph_endpoint, json=graphs_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graphs_config, headers=headers)
+# print(response.text)
+
+pixels_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{READING_GRAPH}"
+
+pixels_config = {
+    "date": "20220815",
+    "quantity": "36.2",
+}
+
+response = requests.post(url=pixels_endpoint, json=pixels_config, headers=headers)
 print(response.text)
+
